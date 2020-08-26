3ed451f33a6d4c3e0ea9e3dd9d3af91b2a189bb7
jerry <2871382340@qq.com>
2020-08-26 23:43:41 +0800

    --user=海哥 Ribbon核心组件IRule 把负载均衡的轮循改为了随机

--- cloud-consumer-order80/src/main/java/myrule/MySelfRule.java
+++ cloud-consumer-order80/src/main/java/myrule/MySelfRule.java
@@ -1,4 +1,16 @@
 package myrule;
 
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class MySelfRule {
+    @Bean
+    public IRule myRule(){
+        //定义为随机的
+        return new RandomRule();
+    }
 }
